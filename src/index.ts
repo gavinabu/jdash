6d44db544eedb49e47f34c4a3d7e65f8f57644c0
Gavin Abu-Zahra <gavinabuzahra@justwhatever.net>
2025-02-03 02:10:54 -0500

    First commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import JHeadInstance from "./accessory/Instance";
 import LoadingMenu from "./accessory/menus/Loading";
 import MainMenu from "./accessory/menus/Main";
-
+import {turnDisplayOn} from "./util/Display";
+turnDisplayOn(); // for testing
 let currentInstance = new JHeadInstance((document.getElementById("display") as HTMLCanvasElement).getContext("2d")!);
 (async () => {
   currentInstance.menu = new LoadingMenu();
